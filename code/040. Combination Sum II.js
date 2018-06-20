fbc1a33989ca4892590e98ff56e8f1e8d8e3f53b
fadaixiaohai <271246498@qq.com>
2018-06-20 17:03:36 +0800

    update 040

--- code/040. Combination Sum II.js
+++ code/040. Combination Sum II.js
@@ -3,11 +3,12 @@
  * @param {number} target
  * @return {number[][]}
  */
-var combinationSum = function(candidates, target) {
+var combinationSum2 = function(candidates, target) {
 
     candidates = candidates.sort((a,b) => a-b);
     var result = [];
     dfs(candidates,target,0,result,[]);
+    console.log(result);
     return result;
 };
 
@@ -20,12 +21,12 @@ var dfs = function (candidates, target, s, result, temp) {
     }
 
     for (var i = s; i < candidates.length; ++i) {
-        if (target < candidates[i]) break;
+        if (i > s && candidates[i] === candidates[i-1]) continue;// 同一层次不能出现相同元素
+        if (target < candidates[i]) break;//剪枝
         temp.push(candidates[i]);
-        dfs(candidates,target-candidates[i],i,result,temp);
-        console.log(temp);
+        dfs(candidates,target-candidates[i],i+1,result,temp);
         temp.pop();
     }
-}
+};
 
-combinationSum([2,3,6,7],7);
+combinationSum2([10,1,2,7,6,1,5],8)
