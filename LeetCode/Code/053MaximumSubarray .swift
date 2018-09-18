02f2490c48c83c8be207dd77d17ffd51d63909d6
fadaixiaohai <271246498@qq.com>
2018-09-18 16:34:13 +0800

    add code

---
+++ LeetCode/Code/053MaximumSubarray .swift
@@ -0,0 +1,16 @@
+//
+//  053Maximum Subarray .swift
+//  LeetCode
+//
+//  Created by Ekko on 2018/9/18.
+//  Copyright © 2018年 fadaixiaohai. All rights reserved.
+//
+
+import Foundation
+
+class MaximumSubarraySolution {
+    func maxSubArray(_ nums: [Int]) -> Int {
+        
+        return 1
+    }
+}
