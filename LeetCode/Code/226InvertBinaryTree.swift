244faf5ae0334fd2db9ff2b2f53b681d49c38b74
fadaixiaohai <271246498@qq.com>
2018-12-28 17:53:16 +0800

    commit 226

---
+++ LeetCode/Code/226InvertBinaryTree.swift
@@ -0,0 +1,28 @@
+//
+//  226InvertBinaryTree.swift
+//  LeetCode
+//
+//  Created by Ekko on 2018/12/28.
+//  Copyright © 2018 fadaixiaohai. All rights reserved.
+//
+
+import Foundation
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     public var val: Int
+ *     public var left: TreeNode?
+ *     public var right: TreeNode?
+ *     public init(_ val: Int) {
+ *         self.val = val
+ *         self.left = nil
+ *         self.right = nil
+ *     }
+ * }
+ */
+class InvertTreeSolution {
+    func invertTree(_ root: TreeNode?) -> TreeNode? {
+        
+    }
+}
