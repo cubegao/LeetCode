de45b8a7861aa207a4d7e78eaef49037a5662e6c
fadaixiaohai <271246498@qq.com>
2019-01-03 23:10:23 +0800

    commit stash

---
+++ LeetCode/ForOffer/06.重建二叉树.swift
@@ -0,0 +1,19 @@
+//
+//  06.重建二叉树.swift
+//  LeetCode
+//
+//  Created by Gaowz on 2019/1/3.
+//  Copyright © 2019 fadaixiaohai. All rights reserved.
+//
+
+import Foundation
+
+class For06Solution {
+    func rebuildTree(_ preOrder: [Int], _ inOrder: [Int] ) -> TreeNode? {
+        
+        let rootValue = preOrder[0]
+        var tree = TreeNode(rootValue)
+        
+        
+    }
+}
