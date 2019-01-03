94f3047d9b06761e67edbb92ec64067f0c7deaf9
fadaixiaohai <271246498@qq.com>
2019-01-03 21:52:20 +0800

    commit 04.替换空格

---
+++ LeetCode/ForOffer/04.替换空格.swift
@@ -0,0 +1,41 @@
+//
+//  04.替换空格.swift
+//  LeetCode
+//
+//  Created by Gaowz on 2019/1/3.
+//  Copyright © 2019 fadaixiaohai. All rights reserved.
+//
+
+import Foundation
+
+class For04Solution {
+    func replace(_ s: String) -> String {
+        
+        var nullNum = 0
+        for c in s {
+            if c == " " {
+                nullNum += 1
+            }
+        }
+        
+        let len = s.count + nullNum * 2
+        var list = Array<Character>(repeating: "x", count: len)
+        
+        var listIndex = len - 1
+        for (_,c) in s.enumerated().reversed() {
+            if c == " " {
+                list[listIndex] = "0"
+                listIndex -= 1
+                list[listIndex] = "2"
+                listIndex -= 1
+                list[listIndex] = "%"
+                listIndex -= 1
+            }else {
+                list[listIndex] = c
+                listIndex -= 1
+            }
+        }
+        
+        return String(list)
+    }
+}
--- LeetCode/Utils/main.swift
+++ LeetCode/Utils/main.swift
@@ -9,6 +9,6 @@
 import Foundation
 
 
-let res = For03Solution().find([[1,2,3,4],[5,6,7,8],[9,10,11,12]], 100)
+let res = For04Solution().replace("We are happy")
 print(res)
 
